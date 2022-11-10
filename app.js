1f7fd613ec03021d11bc7e8b953ac0d5c4acb4a0
xodia117 <callum.jones@ncl-coll.ac.uk>
2022-11-10 10:38:16 +0000

    update 1.14

--- app.js
+++ app.js
@@ -6,17 +6,7 @@ function redirect() {
 // Path: checkout.html
 
 
-function validateForm() {
-    let x = document.forms["signUp"]["fname","lname","email","pword"].value;
-    if (x == "") {
-      alert("Missing fields must be filled out");
-      return false;
-    }
-    else {
-      alert('Thank you for signing up!');
-        window.location.href = "orderComplete.html";
-    }
-  }
+
 
   function submitRedirect() {
     alert('Thank you for submitting your review! You will now be redirected.');
