52f8d5166330e243ca3f343a5f18cc53fcd7f089
xodia117 <callum.jones@ncl-coll.ac.uk>
2022-11-10 12:40:09 +0000

    update attempt 1.5

--- app.js
+++ app.js
@@ -8,6 +8,37 @@ function redirect() {
 
 
 
+document.getElementById("submit").addEventListener("click", validateForm);
+
+function validateForm() {
+  let x = document.getElementById("signUp");
+
+    if (x == "") {
+      alert("Missing fields must be filled out");
+        return false;
+}
+
+else {
+      alert('Thank you for signing up!');
+      window.location.href = "orderComplete.html";
+}
+
+}
+
+// function validateForm() {
+//     let x = document.forms["signUp"]["fname","lname","email","pword"].value;
+//       if (x == "") {
+//         alert("Missing fields must be filled out");
+//       return false;
+//     }
+
+//     else {
+//       alert('Thank you for signing up!');
+//       window.location.href = "orderComplete.html";
+//     }
+//   }
+
+
   function submitRedirect() {
     alert('Thank you for submitting your review! You will now be redirected.');
     window.location.href = "index.html";
