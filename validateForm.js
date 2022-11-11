a78eb93542dd57cf576c85d79179e271e257af75
xodia117 <callum.jones@ncl-coll.ac.uk>
2022-11-11 15:22:51 +0000

    update 1.171

--- validateForm.js
+++ validateForm.js
@@ -1,17 +1,34 @@
-function orderRedirect() {
-    alert('Thank you for signing up! You will now be redirected.');
-    window.location.href = "orderComplete.html";
-  }
 
-function validateForm() {
-    let x = document.forms["signUp"]["fname","lname","email","pword"].value;
-      if (x == "") {
-        alert("Missing fields must be filled out");
-      return false;
-    }
-    else {
+// function validateForm() {
+//     let x = document.forms["signUp"]["fname","lname","email","pword"].value;
+//       if (x == "") {
+//         alert("Missing fields must be filled out");
+//       return false;
+//     }
+//     else {
       // alert('Thank you for signing up!');
       // window.location.href = "orderComplete.html";
-      orderRedirect()
-    }
+//       orderRedirect()
+//     }
+//   }
+
+  // function orderRedirect() {
+  //   alert('Thank you for signing up! You will now be redirected.');
+  //   window.location.href = "orderComplete.html";
+  // }
+
+
+const signUp = document.querySelector(".signUp")
+
+signUp.addEventListener ('click', e => {
+  let x = document.forms["signUp"]["fname","lname","email","pword"].value;
+  if (x == "") {
+    alert("Missing fields must be filled out");
+  return false;
   }
+  else {
+    alert('Thank you for signing up!');
+    window.location.href = "orderComplete.html";
+  }
+})
+
