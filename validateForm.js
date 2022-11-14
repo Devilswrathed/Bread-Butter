e3d1d76592a9272b8cecae96934af7b893d8f09b
xodia117 <callum.jones@ncl-coll.ac.uk>
2022-11-14 14:25:34 +0000

    1.182

--- validateForm.js
+++ validateForm.js
@@ -3,6 +3,7 @@ document.getElementById('signUp').addEventListener('submit', validateForm());
 function validateForm() {
     let x = document.forms["signUp"]["fname","lname","email","pword"].value;
       if (x == "") {
+        document.signUp.x.style.background='Yellow';
         alert("Missing fields must be filled out");
       return false;
     }
