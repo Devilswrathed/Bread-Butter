4864642dc9763ad4a7fc1bb23eeeb643e931aa3c
xodia117 <callum.jones@ncl-coll.ac.uk>
2022-11-14 14:14:42 +0000

    testing 1.18

--- validateForm.js
+++ validateForm.js
@@ -1,34 +1,17 @@
+document.getElementById('signUp').addEventListener('submit', validateForm());
 
-// function validateForm() {
-//     let x = document.forms["signUp"]["fname","lname","email","pword"].value;
-//       if (x == "") {
-//         alert("Missing fields must be filled out");
-//       return false;
-//     }
-//     else {
-      // alert('Thank you for signing up!');
-      // window.location.href = "orderComplete.html";
-//       orderRedirect()
-//     }
-//   }
-
-  // function orderRedirect() {
-  //   alert('Thank you for signing up! You will now be redirected.');
-  //   window.location.href = "orderComplete.html";
-  // }
-
-
-const signUp = document.getElementsByName("signUp")
-
-signUp.addEventListener ('click', e => {
-  let x = document.forms["signUp"]["fname","lname","email","pword"].value;
-  if (x == "") {
-    alert("Missing fields must be filled out");
-  return false;
+function validateForm() {
+    let x = document.forms["signUp"]["fname","lname","email","pword"].value;
+      if (x == "") {
+        alert("Missing fields must be filled out");
+      return false;
+    }
+    else {
+      alert('Thank you for signing up!');
+      window.location.href = "orderComplete.html";
+      orderRedirect()
+    }
   }
-  else {
-    alert('Thank you for signing up!');
-    window.location.href = "orderComplete.html";
-  }
-})
+
+
 
